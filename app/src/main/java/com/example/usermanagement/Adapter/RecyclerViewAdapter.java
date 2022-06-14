cb3f55bc0207c4fd328d4414ae6f59d0ecef8d02
dinhthaison <dinhthaison2000@gmail.com>
2022-06-14 17:36:33 +0700

    Initial commit

---
+++ app/src/main/java/com/example/usermanagement/Adapter/RecyclerViewAdapter.java
@@ -0,0 +1,87 @@
+package com.example.usermanagement.Adapter;
+
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.usermanagement.Model.Users;
+import com.example.usermanagement.R;
+
+import java.util.List;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ItemUserHolder> {
+    Context context;
+    List<Users> usersList;
+
+    public RecyclerViewAdapter(Context context, List<Users> usersList) {
+        this.context = context;
+        this.usersList = usersList;
+    }
+
+    @NonNull
+    @Override
+    public ItemUserHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ItemUserHolder(LayoutInflater.from(context).inflate(R.layout.user_items, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemUserHolder holder, @SuppressLint("RecyclerView") int position) {
+        holder.txtName.setText(usersList.get(position).getName());
+        holder.txtPhone.setText(usersList.get(position).getPhone());
+        holder.txtAddress.setText(usersList.get(position).getAddress());
+        holder.btnRemove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+                builder.setMessage("You want to delete " +"'"+ usersList.get(position).getName() +"'" + " ?");
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        usersList.remove(position);
+                        notifyDataSetChanged();
+                    }
+                });
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return usersList.size();
+    }
+
+    public void addUser(Users users){
+        usersList.add(users);
+        notifyDataSetChanged();
+    }
+
+    public class ItemUserHolder extends RecyclerView.ViewHolder {
+        private TextView txtName, txtPhone, txtAddress;
+        private Button btnRemove;
+        public ItemUserHolder(@NonNull View itemView) {
+            super(itemView);
+            txtName = itemView.findViewById(R.id.txt_username);
+            txtPhone = itemView.findViewById(R.id.txt_phone);
+            txtAddress = itemView.findViewById(R.id.txt_address);
+            btnRemove = itemView.findViewById(R.id.btn_remove);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/usermanagement/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.usermanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.example.usermanagement.Adapter.RecyclerViewAdapter;
+import com.example.usermanagement.Model.Users;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recUser;
+    private EditText edtName, edtPhone;
+    private Spinner spnAddress;
+    private RecyclerViewAdapter recyclerViewAdapter;
+    public List<Users> usersList;
+    private Button btnAdd;
+    private String[] addresses = {"Ha Noi", "Bac Ninh", "Ha Nam", "Nghe An", "Thanh Hoa", "Hung Yen"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recUser = findViewById(R.id.rec1);
+        btnAdd = findViewById(R.id.btn_add);
+        edtName = findViewById(R.id.edt_name);
+        spnAddress = findViewById(R.id.spn_address);
+        edtPhone = findViewById(R.id.edt_phone);
+
+        usersList = new ArrayList<>();
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, addresses);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spnAddress.setAdapter(adapter);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recUser.setLayoutManager(layoutManager);
+        recyclerViewAdapter = new RecyclerViewAdapter(this, usersList);
+        recUser.setAdapter(recyclerViewAdapter);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = edtName.getText().toString();
+                String phone = edtPhone.getText().toString();
+                String address = spnAddress.getSelectedItem().toString();
+                if (TextUtils.isEmpty(name) || TextUtils.isEmpty(phone) || TextUtils.isEmpty(address)){
+                    Dialog dialog = new Dialog(MainActivity.this);
+                    dialog.setContentView(R.layout.dialog);
+
+                    Button btnOk = dialog.findViewById(R.id.btn_ok);
+                    btnOk.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            dialog.dismiss();
+                        }
+                    });
+                    dialog.show();
+                } else {
+                    Users users = new Users(name, phone, address);
+                    recyclerViewAdapter.addUser(users);
+                }
+
+            }
+        });
+
+
+
+    }
+}
